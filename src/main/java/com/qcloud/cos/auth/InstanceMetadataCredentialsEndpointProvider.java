3874419ca884634f2e69e7ba30067fd92fdf5a44
chengwu <chengwu@tencent.com>
2020-10-14 19:30:48 +0800

    add emr instance meta cred provider instance

--- src/main/java/com/qcloud/cos/auth/InstanceMetadataCredentialsEndpointProvider.java
+++ src/main/java/com/qcloud/cos/auth/InstanceMetadataCredentialsEndpointProvider.java
@@ -13,7 +13,8 @@ public class InstanceMetadataCredentialsEndpointProvider extends CredentialsEndp
 
     public enum Instance {
         CPM("http://bm.metadata.tencentyun.com", "/meta-data/cam/security-credentials"),
-        CVM("http://metadata.tencentyun.com", "/meta-data/cam/security-credentials");
+        CVM("http://metadata.tencentyun.com", "/meta-data/cam/security-credentials"),
+        EMR("http://localhost:2888", "/accesskey");
 
         final String METADATA_SERVICE_URL;
         final String METADATA_CREDENTIALS_RESOURCE;
