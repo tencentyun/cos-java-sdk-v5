667933cdf750fa0383610af7a36d4afec7c65279
markjrzhang <markjrzhang@tencent.com>
2020-12-15 15:10:12 +0800

    update doc process demo

--- src/main/java/com/qcloud/cos/demo/ci/DocJobDemo.java
+++ src/main/java/com/qcloud/cos/demo/ci/DocJobDemo.java
@@ -8,13 +8,11 @@ import com.qcloud.cos.model.ciModel.job.*;
  * 文档预览任务相关demo
  */
 public class DocJobDemo {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // 1 初始化用户身份信息（secretId, secretKey）。
         COSClient client = ClientUtils.getTestClient();
         // 2 调用要使用的方法。
-//        createDocJobs(client);
-        describeMediaJob(client);
-//        describeMediaJobs(client);
+        createDocJobs(client);
     }
 
     /**
@@ -26,19 +24,21 @@ public class DocJobDemo {
         //1.创建任务请求对象
         DocJobRequest request = new DocJobRequest();
         //2.添加请求参数 参数详情请见api接口文档
-        request.setBucketName("markjrzhang-1251704708");
+        request.setBucketName("examplebucket-1250000000");
         DocJobObject docJobObject = request.getDocJobObject();
         docJobObject.setTag("DocProcess");
-        docJobObject.getInput().setObject("2.docx");
-        docJobObject.setQueueId("pc02270c617ae4b6d9b0a52cb1cfce6b3");
+        docJobObject.getInput().setObject("demo.docx");
+        docJobObject.setQueueId("pc02270c617ae4b6d9b0a52cb1c*****");
         DocProcessObject docProcessObject = docJobObject.getOperation().getDocProcessObject();
         docProcessObject.setQuality("100");
         docProcessObject.setZoom("100");
         docProcessObject.setStartPage("1");
-        docProcessObject.setEndPage("-1");
+        docProcessObject.setEndPage("3");
+        docProcessObject.setTgtType("png");
+        docProcessObject.setDocPassword("123");
         MediaOutputObject output = docJobObject.getOperation().getOutput();
         output.setRegion("ap-chongqing");
-        output.setBucket("markjrzhang-1251704708");
+        output.setBucket("examplebucket-1250000000");
         output.setObject("mark/pic-${Page}.jpg");
         //3.调用接口,获取任务响应对象
         DocJobResponse docProcessJobs = client.createDocProcessJobs(request);
@@ -54,8 +54,8 @@ public class DocJobDemo {
         //1.创建任务请求对象
         DocJobRequest request = new DocJobRequest();
         //2.添加请求参数 参数详情请见api接口文档
-        request.setBucketName("markjrzhang-1251704708");
-        request.setJobId("dcb2be7663ddb11eba57c71c6d5ddb08e");
+        request.setBucketName("examplebucket-1250000000");
+        request.setJobId("d75b6ea083df711eb8d09476dfb8*****");
         //3.调用接口,获取任务响应对象
         DocJobResponse docJobResponse = client.describeDocProcessJob(request);
         System.out.println(docJobResponse);
@@ -70,8 +70,8 @@ public class DocJobDemo {
         //1.创建任务请求对象
         DocJobListRequest request = new DocJobListRequest();
         //2.添加请求参数 参数详情请见api接口文档
-        request.setBucketName("markjrzhang-1251704708");
-        request.setQueueId("pc02270c617ae4b6d9b0a52cb1cfce6b3");
+        request.setBucketName("examplebucket-1250000000");
+        request.setQueueId("pc02270c617ae4b6d9b0a52cb1c*****");
         request.setTag("DocProcess");
         request.setStartCreationTime("2020-12-10T16:20:07+0800");
         //3.调用接口,获取任务响应对象
--- src/main/java/com/qcloud/cos/demo/ci/DocQueueDemo.java
+++ src/main/java/com/qcloud/cos/demo/ci/DocQueueDemo.java
@@ -4,13 +4,11 @@ import com.qcloud.cos.COSClient;
 import com.qcloud.cos.model.ciModel.queue.DocListQueueResponse;
 import com.qcloud.cos.model.ciModel.queue.DocQueueRequest;
 
-import java.io.UnsupportedEncodingException;
-
 /**
  * 文档预览队列相关demo https://cloud.tencent.com/document/product/460/46946
  */
 public class DocQueueDemo {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
         // 1 初始化用户身份信息（secretId, secretKey）。
         COSClient client = ClientUtils.getTestClient();
         // 2 调用要使用的方法。
@@ -21,11 +19,11 @@ public class DocQueueDemo {
      * DescribeDocProcessQueues 接口用于查询文档预览队列。
      * @param client
      */
-    public static void describeDocProcessQueues(COSClient client) throws UnsupportedEncodingException {
+    public static void describeDocProcessQueues(COSClient client)  {
         //1.创建任务请求对象
         DocQueueRequest request = new DocQueueRequest();
         //2.添加请求参数 参数详情请见api接口文档
-        request.setBucketName("markjrzhang-1251704708");
+        request.setBucketName("examplebucket-1250000000");
         //3.调用接口,获取任务响应对象
         DocListQueueResponse response = client.describeDocProcessQueues(request);
         System.out.println(response);
@@ -39,14 +37,14 @@ public class DocQueueDemo {
         //1.创建任务请求对象
         DocQueueRequest request = new DocQueueRequest();
         //2.添加请求参数 参数详情请见api接口文档
-        request.setBucketName("markjrzhang-1251704708");
-        request.setQueueId("pc02270c617ae4b6d9b0a52cb1cfce6b3");
+        request.setBucketName("examplebucket-1250000000");
+        request.setQueueId("pc02270c617ae4b6d9b0a52cb1cf****");
         request.getNotifyConfig().setUrl("http://cloud.tencent.com");
         request.getNotifyConfig().setState("On");
         request.getNotifyConfig().setEvent("TransCodingFinish");
         request.getNotifyConfig().setType("Url");
         request.setState("Active");
-        request.setName("mark-3");
+        request.setName("mark");
         //3.调用接口,获取任务响应对象
         boolean result = client.updateDocProcessQueue(request);
         System.out.println(result);
