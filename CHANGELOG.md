6c02a905efec7c0a214eb97f0efe14033428eeda
aldoli <aldoli@tencent.com>
2021-03-04 15:18:29 +0800

    supplement the required info for ces-kms encryption client

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+## [5.6.37] - 2021-3-10
+
+### Added
+- add KMS Encryption Client support.
+
 ## [5.6.24] - 2020-6-3
 
 ### Added
--- src/main/java/com/qcloud/cos/internal/crypto/QCLOUDKMS.java
+++ src/main/java/com/qcloud/cos/internal/crypto/QCLOUDKMS.java
@@ -18,6 +18,42 @@
 
 package com.qcloud.cos.internal.crypto;
 
+import com.tencentcloudapi.kms.v20190118.models.DecryptRequest;
+import com.tencentcloudapi.kms.v20190118.models.DecryptResponse;
+import com.tencentcloudapi.kms.v20190118.models.EncryptRequest;
+import com.tencentcloudapi.kms.v20190118.models.EncryptResponse;
+import com.tencentcloudapi.kms.v20190118.models.GenerateDataKeyRequest;
+import com.tencentcloudapi.kms.v20190118.models.GenerateDataKeyResponse;
+
 public interface QCLOUDKMS {
 
+    /**
+    * Generates a unique symmetric data key for client-side encryption. This operation returns a plaintext copy of the
+    * data key and a copy that is encrypted under a customer master key (CMK) that you specify. You can use the
+    * plaintext key to encrypt your data outside of KMS and store the encrypted data key with the encrypted data.
+    */
+    GenerateDataKeyResponse generateDataKey(GenerateDataKeyRequest generateDataKeyRequest);
+
+    /**
+    * Encrypts plaintext into ciphertext by using a customer master key (CMK).
+    */
+    EncryptResponse encrypt(EncryptRequest encryptRequest);
+    
+    /**
+    * Decrypts ciphertext that was encrypted by a KMS customer master key (CMK) using any of the following
+    * operations:
+    *
+    * encrypt
+    *
+    * generateDataKey
+    *
+    */
+    DecryptResponse decrypt(DecryptRequest decryptRequest);
+
+    /**
+     * Shuts down this client object, releasing any resources that might be held open. This is an optional method, and
+     * callers are not expected to call it, but can if they want to explicitly release any open resources. Once a client
+     * has been shutdown, it should not be used to make any more requests.
+     */
+    void shutdown();
 }
