a7e33082b37212e6320cfb014d25c847997f9617
aldoli <aldoli@tencent.com>
2021-03-18 11:21:53 +0800

    add some demos

--- src/main/java/com/qcloud/cos/demo/TransferManagerDemo.java
+++ src/main/java/com/qcloud/cos/demo/TransferManagerDemo.java
@@ -15,17 +15,17 @@ import com.qcloud.cos.model.CopyResult;
 import com.qcloud.cos.model.GetObjectRequest;
 import com.qcloud.cos.model.PutObjectRequest;
 import com.qcloud.cos.model.UploadResult;
-
 import com.qcloud.cos.region.Region;
 import com.qcloud.cos.transfer.Copy;
 import com.qcloud.cos.transfer.Download;
+import com.qcloud.cos.transfer.MultipleFileUpload;
 import com.qcloud.cos.transfer.PersistableDownload;
 import com.qcloud.cos.transfer.PersistableUpload;
 import com.qcloud.cos.transfer.Transfer;
 import com.qcloud.cos.transfer.TransferManager;
+import com.qcloud.cos.transfer.TransferManagerConfiguration;
 import com.qcloud.cos.transfer.TransferProgress;
 import com.qcloud.cos.transfer.Upload;
-import com.qcloud.cos.transfer.TransferManagerConfiguration;
 
 // TransferManager提供异步的上传文件, 下载文件，copy文件的高级API接口
 // 可以根据文件大小自动的选择上传接口或者copy接口,方便用户使用, 无需自行封装较复杂的分块上传或者分块copy
@@ -43,7 +43,7 @@ public class TransferManagerDemo {
             long so_far = progress.getBytesTransferred();
             long total = progress.getTotalBytesToTransfer();
             double pct = progress.getPercentTransferred();
-            System.out.printf("[%d / %d]\n", so_far, total);
+            System.out.printf("[%d / %d] = %.02f%%\n", so_far, total, pct);
         } while (transfer.isDone() == false);
         System.out.println(transfer.getState());
     }
@@ -116,6 +116,51 @@ public class TransferManagerDemo {
             showTransferProgress(upload);
             UploadResult uploadResult = upload.waitForUploadResult();
             System.out.println(uploadResult.getETag());
+        } catch (CosServiceException e) {
+            e.printStackTrace();
+        } catch (CosClientException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        transferManager.shutdownNow();
+        cosclient.shutdown();
+    }
+
+    // 批量上传
+    public static void uploadDirectory() {
+        // 1 初始化用户身份信息(secretId, secretKey)
+        COSCredentials cred = new BasicCOSCredentials("AKIDXXXXXXXX", "1A2Z3YYYYYYYYYY");
+        // 2 设置bucket的区域, COS地域的简称请参照 https://www.qcloud.com/document/product/436/6224
+        ClientConfig clientConfig = new ClientConfig(new Region("ap-beijing-1"));
+        // 3 生成cos客户端
+        COSClient cosclient = new COSClient(cred, clientConfig);
+        // bucket名需包含appid
+        String bucketName = "mybucket-1251668577";
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(4);
+        // 传入一个threadpool, 若不传入线程池, 默认TransferManager中会生成一个单线程的线程池。
+        TransferManager transferManager = new TransferManager(cosclient, threadPool);
+
+        // 设置文件上传到 bucket 之后的前缀目录，设置为 “”，表示上传到 bucket 的根目录
+        String cos_path = "/prefix";
+        // 要上传的文件夹的绝对路径
+        String dir_path = "/to/mydir";
+        // 是否递归上传目录下的子目录，如果是 true，子目录下的文件也会上传，且cos上会保持目录结构
+        Boolean recursive = false;
+
+        try {
+            // 返回一个异步结果Upload, 可同步的调用waitForUploadResult等待upload结束, 成功返回UploadResult, 失败抛出异常.
+            MultipleFileUpload upload = transferManager.uploadDirectory(bucketName, cos_path, new File(dir_path), recursive);
+
+            // 可以选择查看上传进度
+            showTransferProgress(upload);
+
+            // 或者阻塞等待完成
+            upload.waitForCompletion();
+
+            System.out.println("upload directory done.");
         } catch (CosServiceException e) {
             e.printStackTrace();
         } catch (CosClientException e) {
