4414bbb48045965084091032f20d15105fe8214a
gaoyd <yangdonggao@tencent.com>
2023-04-13 15:40:05 +0800

    llt

--- src/test/java/com/qcloud/cos/region/RegionTest.java
+++ src/test/java/com/qcloud/cos/region/RegionTest.java
@@ -5,16 +5,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class RegionTest {
-
-    @Test
-    public void getDisplayName() {
-    }
-
     /**
      * 判定相等是按region name，而不是按display name
      */
     @Test
     public void testEquals() {
+        Region region  = new Region("only", "only");
+        assertEquals(region, region);
+
         assertEquals(new Region("region_name", "display1")
                 , new Region("region_name", "display2"));
     }
@@ -23,5 +21,21 @@ public class RegionTest {
     public void testNotEquals() {
         assertNotEquals(new Region("region_name1", "display")
                 , new Region("region_name2", "display"));
+
+        assertNotEquals(new Region("region_name1", "display")
+                , new Object());
+    }
+
+    @Test
+    public void testDispaly() {
+        Region region  = new Region("cn-east", "display");
+        assertEquals("cn-east", Region.formatRegion(region));
+    }
+
+
+    @Test
+    public void testFormatRegion() {
+        Region region  = new Region("only", "display");
+        assertEquals("display", region.getDisplayName());
     }
 }
