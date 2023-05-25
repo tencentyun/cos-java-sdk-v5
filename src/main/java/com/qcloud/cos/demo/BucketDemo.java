37f2ee89d3374bedf984bf21f8180044040e07f2
jeffreykzli <jeffreykzli@tencent.com>
2023-05-25 16:21:51 +0800

    更新创多AZ桶demo

--- src/main/java/com/qcloud/cos/demo/BucketDemo.java
+++ src/main/java/com/qcloud/cos/demo/BucketDemo.java
@@ -179,7 +179,7 @@ public class BucketDemo {
         CreateBucketRequest createBucketRequest = new CreateBucketRequest(bucketname);
 
         try {
-            cosclient.createBucket(createBucketRequest);
+            cosclient.createBucket(createBucketRequest, true);
         } catch (CosServiceException cse) {
             cse.printStackTrace();
         } catch (CosClientException cce) {
