7ae17280bb712a1dbd8f40b4006d2db06b4ed669
markjrzhang <markjrzhang@tencent.com>
2024-03-29 18:03:32 +0800

    update demo

--- src/main/java/com/qcloud/cos/demo/ci/GetHLSPlayKeyDemo.java
+++ src/main/java/com/qcloud/cos/demo/ci/GetHLSPlayKeyDemo.java
@@ -1,7 +1,6 @@
 package com.qcloud.cos.demo.ci;
 
 import com.qcloud.cos.COSClient;
-import com.qcloud.cos.model.ciModel.hls.GetHLSPlayKey;
 import com.qcloud.cos.model.ciModel.hls.GetHLSPlayKeyRequest;
 import com.qcloud.cos.model.ciModel.hls.GetHLSPlayKeyResponse;
 import com.qcloud.cos.utils.Jackson;
@@ -12,18 +11,6 @@ import java.util.List;
  * 获取hls播放密钥 详情见暂无
  */
 public class GetHLSPlayKeyDemo {
-
-    public static void main(String[] args) {
-
-        // 初始化用户身份信息(secretId, secretKey)
-        COSCredentials cred = new BasicCOSCredentials("AKIDXXXXXXXX","1A2Z3YYYYYYYYYY");
-        // 设置bucket的区域, COS地域的简称请参照 https://www.qcloud.com/document/product/436/6224
-        ClientConfig clientConfig = new ClientConfig(new Region("ap-guangzhou"));
-        // 生成cos客户端
-        COSClient client = new COSClient(cred, clientConfig);
-        // 2 调用要使用的方法。
-        getHLSPlayKey(client);
-    }
 
     /**
      * getHLSPlayKey 该接口用于获取hls播放密钥。
