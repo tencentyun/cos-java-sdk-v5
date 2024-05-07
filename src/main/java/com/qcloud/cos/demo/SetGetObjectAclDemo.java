e23ca312597f2dd581c95dcff3a9fdbc85be1a22
jeffreykzli <jeffreykzli@tencent.com>
2024-05-07 20:44:03 +0800

    update demo

--- src/main/java/com/qcloud/cos/demo/SetGetObjectAclDemo.java
+++ src/main/java/com/qcloud/cos/demo/SetGetObjectAclDemo.java
@@ -17,7 +17,7 @@ import com.qcloud.cos.region.Region;
 public class SetGetObjectAclDemo {
     public static void setGetObjectAclTest() {
         // 1 初始化用户身份信息(secretId, secretKey)
-	    COSCredentials cred = new BasicCOSCredentials("AKIDXXXXXXXX", "1A2Z3YYYYYYYYYY");
+	COSCredentials cred = new BasicCOSCredentials("AKIDXXXXXXXX", "1A2Z3YYYYYYYYYY");
         // 2 设置bucket的区域, COS地域的简称请参照 https://www.qcloud.com/document/product/436/6224
         ClientConfig clientConfig = new ClientConfig(new Region("ap-guangzhou"));
         // 3 生成cos客户端
