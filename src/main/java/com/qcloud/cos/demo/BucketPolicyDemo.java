afaed132cc0bc218fc258fbda2f2edc76a1f2aee
jeffreykzli <jeffreykzli@tencent.com>
2024-05-17 17:05:33 +0800

    update bucket policy Demo

--- src/main/java/com/qcloud/cos/demo/BucketPolicyDemo.java
+++ src/main/java/com/qcloud/cos/demo/BucketPolicyDemo.java
@@ -10,16 +10,16 @@ import com.qcloud.cos.model.BucketPolicy;
 
 public class BucketPolicyDemo {
     public static void main(String[] args) {
-        setGetBucketPolicy();
+        setGetDelBucketPolicy();
     }
 
-    private static void setGetBucketPolicy() {
+    private static void setGetDelBucketPolicy() {
         // 1 初始化用户身份信息(secretId, secretKey)
         COSCredentials cred = new BasicCOSCredentials("SECRET_ID", "SECRET_KEY");
         // 2 设置bucket的区域, COS地域的简称请参照 https://www.qcloud.com/document/product/436/6224
         ClientConfig clientConfig = new ClientConfig(new Region("ap-guangzhou"));
         // 3 生成cos客户端
-        COSClient cosclient = new COSClient(cred, clientConfig);
+        COSClient cosClient = new COSClient(cred, clientConfig);
         // bucket名需包含appid
         String bucketName = "examplebucket-1250000000";
         String bucketPolicyStr = "{" +
@@ -41,9 +41,12 @@ public class BucketPolicyDemo {
                 "  ]," +
                 "  \"version\": \"2.0\"" +
                 "}";
-        cosclient.setBucketPolicy(bucketName, bucketPolicyStr);
-        BucketPolicy bucketPolicy = cosclient.getBucketPolicy(bucketName);
+        cosClient.setBucketPolicy(bucketName, bucketPolicyStr);
+        BucketPolicy bucketPolicy = cosClient.getBucketPolicy(bucketName);
         System.out.println(bucketPolicy.getPolicyText());
+
+        cosClient.deleteBucketPolicy(bucketName);
+        System.out.println("bucket policy has been deleted");
     }
 }
 
