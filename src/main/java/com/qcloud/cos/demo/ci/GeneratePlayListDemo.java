00e80303f68a10c453e532f078752da5b5869581
markjrzhang <markjrzhang@tencent.com>
2024-07-16 09:23:58 +0800

    update demo

--- src/main/java/com/qcloud/cos/demo/ci/GeneratePlayListDemo.java
+++ src/main/java/com/qcloud/cos/demo/ci/GeneratePlayListDemo.java
@@ -1,6 +1,10 @@
 package com.qcloud.cos.demo.ci;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.qcloud.cos.COSClient;
+import com.qcloud.cos.http.HttpMethodName;
 import com.qcloud.cos.model.ciModel.common.MediaVod;
 import com.qcloud.cos.model.ciModel.job.*;
 import com.qcloud.cos.model.ciModel.job.v2.GetPlayListRequest;
@@ -9,7 +13,14 @@ import com.qcloud.cos.model.ciModel.job.v2.MediaJobsRequestV2;
 import com.qcloud.cos.utils.Jackson;
 
 import java.io.*;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 媒体处理 边转边播接口相关demo
@@ -66,40 +77,70 @@ public class GeneratePlayListDemo {
         System.out.println(Jackson.toJsonString(response));
     }
 
-    /**
-     * getPlayList 接口用于获取私有 M3U8 ts 资源的下载授权
-     */
+    private static String appId = "123456789";
+    private static String bucket = "demo-123456789";
+    private static String objectKey = "test.m3u8";
+    private static String expires = "3600";
+    private static byte[] secret = "YourSecret".getBytes();
+
     public static void getPlayList(COSClient client)  {
-        GetPlayListRequest request = new GetPlayListRequest();
-        request.setBucketName("demo-1234567890");
-        request.setObject("output/media/test.m3u8");
-        request.setExpires("3600");
-        try {
-            InputStream response = client.getPlayList(request);
-            System.out.println(inputStreamToString(response));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String bucketName = bucket;
+        String key = objectKey;
+        // 设置签名过期时间(可选), 若未进行设置则默认使用 ClientConfig 中的签名过期时间(1小时)
+        Instant now = Instant.now();
+        Instant expire = now.plus(Long.parseLong(expires), ChronoUnit.SECONDS);
+        Date expirationDate = Date.from(expire);
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("ci-process", "getplaylist");
+        params.put("expires", "43200");
+        Map<String, String> headers = new HashMap<String, String>();
+
+        HttpMethodName method = HttpMethodName.GET;
+        URL url = client.generatePresignedUrl(bucketName, key, expirationDate, method, headers, params);
+        System.out.println(url.toString());
     }
 
-    public static String inputStreamToString(InputStream inputStream) throws IOException {
-        if (inputStream == null) {
-            return "";
-        }
+public static void getPlayListSimple(COSClient client) throws UnsupportedEncodingException {
+    String bucketName = bucket;
+    String key = objectKey;
+    // 设置签名过期时间(可选), 若未进行设置则默认使用 ClientConfig 中的签名过期时间(1小时)
+    Instant now = Instant.now();
+    Instant expire = now.plus(Long.parseLong(expires), ChronoUnit.SECONDS);
+    Date expirationDate = Date.from(expire);
+    String token = generateToken(appId, bucket, objectKey, secret, expirationDate);
+    Map<String, String> params = new HashMap<String, String>();
+    params.put("ci-process", "getplaylist");
+    params.put("expires", "43200");
+    params.put("token-type", "JwtToken");
+    params.put("token",token);
+    Map<String, String> headers = new HashMap<String, String>();
 
-        StringBuilder stringBuilder = new StringBuilder();
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+    HttpMethodName method = HttpMethodName.GET;
+    URL url = client.generatePresignedUrl(bucketName, key, expirationDate, method, headers, params);
+    System.out.println(url.toString());
+}
 
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            stringBuilder.append(line);
-        }
+public static String generateToken(String appId, String bucketId, String objectKey, byte[] secret, Date expires) throws UnsupportedEncodingException {
+    Instant now = Instant.now();
+    String encodedObjectKey;
+    encodedObjectKey = URLEncoder.encode(objectKey, "UTF-8");
 
-        bufferedReader.close();
-        inputStreamReader.close();
-        inputStream.close();
+    Algorithm algorithm = Algorithm.HMAC256(secret);
+    JWTCreator.Builder builder = JWT.create().withIssuer("client")
+            .withIssuedAt(Date.from(now))
+            .withExpiresAt(expires)
+            .withClaim("Type", "CosCiToken")
+            .withClaim("AppId", appId)
+            .withClaim("BucketId", bucketId)
+            .withClaim("Object", encodedObjectKey)
+            .withClaim("Issuer", "client")
+            .withClaim("IssuedTimeStamp", now.getEpochSecond())
+            .withClaim("ExpireTimeStamp", expires.getTime()/1000)
+            .withClaim("UsageLimit", 20)
+            .withClaim("ProtectSchema", "rsa1024")
+//          .withClaim("PublicKey", "xxx")
+            .withClaim("ProtectContentKey", 0);
+    return builder.sign(algorithm);
+}
 
-        return stringBuilder.toString();
-    }
 }
